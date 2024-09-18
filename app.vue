06c09f8acea424513833a89322fdf177214c50ed
Felipe Bona <felipechiodinibona@hotmail.com>
2024-09-17 21:56:43 -0300

    ajusts

--- app.vue
+++ app.vue
@@ -4,11 +4,11 @@
     <h1 class="text-3xl my-3 text-red-500">Developer</h1>
     <h2 class="text-2xl mb-4">Felipe Chiodini Bona</h2>
     <div class="flex gap-3 justify-center">
-      <div class="bg-red-500 w-1/2 md:w-1/3 shadow-md shadow-slate-300 p-3 md:p-6">
+      <div class="bg-red-500 w-2/3 md:w-1/3 shadow-md shadow-slate-300 p-3 md:p-6">
         <img src="https://via.placeholder.com/150" class="mx-auto rounded-full my-6" alt="foto de uma pessoa">
         <h2 class="text-xl md:text-2xl">I am happy to know you that 300+ projects done successfully!</h2>
       </div>
-      <div class="grid grid-rows-2 grid-cols-2 gap-4">
+      <div class="grid grid-rows-2 md:grid-cols-2 gap-4 w-1/3">
         <div class="bg-blue-500 shadow-md shadow-slate-300 sh p-4">
           <h6 class="text-xl">Laravel</h6>
         </div>
@@ -21,6 +21,29 @@
         <div class="bg-blue-500 shadow-md shadow-slate-300 sh p-4">
           <h6 class="text-xl">Laravel</h6>
         </div>
+      </div>
+    </div>
+    <div>
+      <h2 class="text-2xl my-4">Projetos</h2>
+      <div>
+        <ul>
+          <li>
+            <a href="https://www.fcbsolucoesweb.com.br">
+              FCB Soluções Web
+            </a>
+          </li>
+          <li>
+            <a href="https://www.rapideats.com.br">
+              RapidEats
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Contador de Calorias
+            </a>
+            <small>Em Breve...</small>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
