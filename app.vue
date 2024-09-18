c88880ba22309f86ea5ec554a992876d14d12ab7
Felipe Bona <felipechiodinibona@hotmail.com>
2024-09-17 21:19:56 -0300

    ajusts

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="text-center p-4">
+    <span>Um pouco sobre mim</span>
+    <h1 class="text-3xl my-3 text-red-500">Developer</h1>
+    <h2 class="text-2xl mb-4">Felipe Chiodini Bona</h2>
+    <div class="flex gap-3 justify-center">
+      <div class="bg-red-500 w-1/2 md:w-1/3 shadow-md shadow-slate-300 p-3 md:p-6">
+        <img src="https://via.placeholder.com/150" class="mx-auto rounded-full my-6" alt="foto de uma pessoa">
+        <h2 class="text-xl md:text-2xl">I am happy to know you that 300+ projects done successfully!</h2>
+      </div>
+      <div class="grid grid-rows-2 grid-cols-2 gap-4">
+        <div class="bg-blue-500 shadow-md shadow-slate-300 sh p-4">
+          <h6 class="text-xl">Laravel</h6>
+        </div>
+        <div class="bg-blue-500 shadow-md shadow-slate-300 sh p-4">
+          <h6 class="text-xl">Laravel</h6>
+        </div>
+        <div class="bg-blue-500 shadow-md shadow-slate-300 sh p-4">
+          <h6 class="text-xl">Laravel</h6>
+        </div>
+        <div class="bg-blue-500 shadow-md shadow-slate-300 sh p-4">
+          <h6 class="text-xl">Laravel</h6>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
